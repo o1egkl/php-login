409c965b5b2db8914ea61bc7cc590462836c1de1
o1egkl <o1egkl@gmail.com>
2015-04-16 01:23:37 +0300

    sadasdas

--- views/stats.php
+++ views/stats.php
@@ -38,7 +38,7 @@
 		?>
 	</table>
 </div>	
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="js/jquery-1.11.2.js"></script>
 <script language="javascript" type="text/javascript">
 
 setInterval(function(){
